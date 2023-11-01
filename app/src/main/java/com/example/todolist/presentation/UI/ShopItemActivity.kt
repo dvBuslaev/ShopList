b333d2690ea16367b87aad999a237b71a9e0d2d7
Dmitriy Buslaev <dvbuslaev@gmail.com>
2023-11-01 16:09:17 +0300

    ShopItemActivity TODO

--- app/src/main/java/com/example/todolist/presentation/UI/ShopItemActivity.kt
+++ app/src/main/java/com/example/todolist/presentation/UI/ShopItemActivity.kt
@@ -2,14 +2,35 @@ package com.example.todolist.presentation.UI
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.parseIntent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.R
+import com.example.todolist.data.ShopItem
+import com.example.todolist.presentation.Model.ShopItemViewModel
+import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var buttonSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+
     }
 
     companion object {
@@ -17,6 +38,7 @@ class ShopItemActivity : AppCompatActivity() {
         private const val MODE_EDIT = "edit_mode"
         private const val MODE_ADD = "add_mode"
         private const val SHOP_ITEM = "shop_item_id"
+        private const val MODE_UNKNOWN = ""
 
         fun setupEditIntent(context: Context, shopItemID: Int): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
@@ -33,6 +55,29 @@ class ShopItemActivity : AppCompatActivity() {
             return intent
 
         }
+    }
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(SHOP_ITEM)) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+            shopItemId = intent.getIntExtra(SHOP_ITEM, ShopItem.UNDEFINED_ID)
+        }
+    }
+    private fun initViews(){
+        tilName=findViewById(R.id.til_name)
+        tilCount=findViewById(R.id.til_count)
+        etName=findViewById(R.id.et_name)
+        etCount=findViewById(R.id.et_count)
+        buttonSave=findViewById(R.id.save_button)
     }
 
 }
